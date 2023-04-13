c97e466aae8655b34d100fe2659590ea8b29f08c
mattialicausi <licausivitomattia@gmail.com>
2023-04-13 11:09:10 +0200

    scaffolding

---
+++ src/test/java/com/java/springilmiofotoalbum/SpringIlMioFotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springilmiofotoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIlMioFotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
